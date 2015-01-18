c71a70064c0b9034a7e89190fb6012da8ec89848
Sakshyam Dahal <dahalsakshyam@gmail.com>
2015-01-18 03:48:54 -0500

    use concurrency and handle error

--- app/src/main/java/com/androidpractice/sakshyamdahal/sunny/MainActivity.java
+++ app/src/main/java/com/androidpractice/sakshyamdahal/sunny/MainActivity.java
@@ -1,13 +1,18 @@
 package com.androidpractice.sakshyamdahal.sunny;
 
 import android.app.DownloadManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -30,20 +35,67 @@ public class MainActivity extends ActionBarActivity {
         String forecastUrl = "https://api.forecast.io/forecast/" +
                 apiKey + "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(forecastUrl).build();
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(forecastUrl).build();
 
-        Call call = client.newCall(request);
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
 
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful())
-            {
-                Log.v(TAG, response.body().string());
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+
+//        try {
+//            // execute() method is for synchronous event
+//            // Response response = call.execute();
+//            if (response.isSuccessful())
+//            {
+//                Log.v(TAG, response.body().string());
+//            }
+//        } catch (IOException e) {
+//            Log.e(TAG, "Exception caught: ", e);
+//        }
         }
+        else
+        {
+            Toast.makeText(this, getString(R.string.network_unavailable_message), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+
+        // network present and connected
+        if (networkInfo != null && networkInfo.isConnected())
+            isAvailable = true;
+
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
 }
