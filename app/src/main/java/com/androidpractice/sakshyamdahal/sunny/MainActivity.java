81341daa7a8e417b85795b96e2d121df6941c578
Sakshyam Dahal <dahalsakshyam@gmail.com>
2015-01-18 02:05:33 -0500

    make http request using OkHttpClient

--- app/src/main/java/com/androidpractice/sakshyamdahal/sunny/MainActivity.java
+++ app/src/main/java/com/androidpractice/sakshyamdahal/sunny/MainActivity.java
@@ -1,39 +1,49 @@
 package com.androidpractice.sakshyamdahal.sunny;
 
+import android.app.DownloadManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        String apiKey = "5534ffc4c4805ecb5a7800e9be7bbe27";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.forecast.io/forecast/" +
+                apiKey + "/" + latitude + "," + longitude;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(forecastUrl).build();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        Call call = client.newCall(request);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful())
+            {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
